5a9b2813133eafaf6504a1e2ad5fbb2b785ac574
John Francis Agustin M. Galindez <galindez.johnfrancisagustin@gmail.com>
2022-09-08 20:18:17 +0800

    first commit

--- services/model_utils.py
+++ services/model_utils.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pandas as pd
-from catboost import CatBoostClassifier,Pool
+from catboost import CatBoostClassifier
 import plotly.graph_objects as go
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'metabolic_syndrome2022-07-10')
 
 # Global variable that stores trained model instance
-MODEL = CatBoostClassifier().load_model('metabolic_syndrome2022-07-10')
+MODEL = CatBoostClassifier().load_model(filename)
 
 # Dictionary that converts frontend feature names to names understood by model
 display_to_model = {'Age':'Age', 'sex':'Sex', 'Marital':'Marital', 'Income':'Income', 'Race':'Race', 'WaistCirc':'WaistCirc',
