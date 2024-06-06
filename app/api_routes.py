b157e38ede7b1121639d083c3070d12886f8ed9b
John Francis Agustin Galindez <jgalindez@chanzglobal.com>
2024-06-06 15:39:25 +0800

    adjusted to be an api

--- app/api_routes.py
+++ app/api_routes.py
@@ -5,16 +5,35 @@ from services.model_utils import predict_hf
 
 bp1 = Blueprint('main', __name__, url_prefix='/main')
 
+
+def check_form_fields(form_data):
+    required_fields = ['sex', 'Marital', 'Income', 'Race', 'WaistCirc', 'Age', 'BMI', 'Albuminuria', 'UrAlbCr', 'UricAcid', 'BloodGlucose', 'HDL', 'Triglycerides']
+    
+    missing_fields = [field for field in required_fields if field not in form_data]
+    
+    if missing_fields:
+        print("The following fields are missing from the form:", missing_fields)
+        return {'is_empty':True , 'missing_fields':missing_fields}
+    else:
+        print("All required fields are present in the form.")
+        return True
+    
 @bp1.route('/api/make_prediction', methods=['POST'])
+
 def make_prediction():
-
-    form_df:pd.DataFrame = pd.DataFrame(request.form, index=[0])
-
-    pred = predict_hf(form_df)
-    print(pred)
-    pred_class_1 = pred[0][0]
+    
+    checkFields = check_form_fields(request.form)
+    if(checkFields['is_empty']):
+        return {'missing_fields':checkFields['missing_fields'] , 'code':'422'}  
+    
+    
+    # form_df:pd.DataFrame = pd.DataFrame(request.form, index=[0])
+    
+    # pred = predict_hf(form_df)
+    # print(pred)
+    # pred_class_1 = pred[0][0]
   
 
 
-    return {'pred':pred_class_1}
+    # return {'pred':pred_class_1}
 
--- app/app.py
+++ app/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 app.register_blueprint(bp1)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
